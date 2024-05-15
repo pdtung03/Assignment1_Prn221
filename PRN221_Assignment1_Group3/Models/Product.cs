b1c8d5acc02cf21416d88b71e5184e7e2f217b5a
pdtung03 <TungPDHE173492@fpt.edu.vn>
2024-05-15 15:01:33 +0700

    upload file

---
+++ PRN221_Assignment1_Group3/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN221_Assignment1_Group3.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public int UnitPrice { get; set; }
+
+        public virtual Category Category { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ PRN221_Assignment1_Group3/Models/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN221_Assignment1_Group3.Models
+{
+    public partial class Staff
+    {
+        public Staff()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int StaffId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Role { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
